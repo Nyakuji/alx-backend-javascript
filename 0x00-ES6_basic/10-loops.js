b71c25ed2755e009f0a5c139ce2ace3bdfa2beff
philip Opiyo <opiyophilipo@gmail.com>
2024-04-26 12:49:07 +0300

    Fix indentation in appendToEachArrayValue function

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const Array = [];
-    for (const value of array) {
-      Array.push(appendString + value);
-    }
-  
-    return Array;
+  const Array = [];
+  for (const value of array) {
+    Array.push(appendString + value);
   }
+
+  return Array;
+}
