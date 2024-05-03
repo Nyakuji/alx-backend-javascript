88489d4d1a50442d948be13661ede798f29e6313
philip Opiyo <opiyophilipo@gmail.com>
2024-05-03 18:14:33 +0300

    refactor: Update teacher's last name in main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -2,8 +2,8 @@ export const cpp: Subjects.Cpp = new Subjects.Cpp();
 export const java: Subjects.Java = new Subjects.Java();
 export const react: Subjects.React = new Subjects.React();
 export const cTeacher: Subjects.Teacher = {
-  firstName: 'Dennis',
-  lastName: 'Ritchie',
+  firstName: 'Philip',
+  lastName: 'Nyakuji',
   experienceTeachingC: 10,
 };
 
