21c7cbad5500e731c3b575aa614348c80be85701
philip Opiyo <opiyophilipo@gmail.com>
2024-05-05 18:03:15 +0300

    refactor: Improve createInt8TypedArray function

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,17 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
+  if (position >= length) {
     throw new Error('Position outside range');
   }
-
-  // create  a new ArrayBuffer with specified lenghth
-  const buffer = new ArrayBuffer(length);
-
-  // Use Dataview to manipulate the ArrayBuffer
-  const view = new DataView(buffer);
-
-  // set the Int8 value at specified position
-  view.setInt8(position, value);
-
-  // return modified ArrayBuffer
+  const buffer = new DataView(new ArrayBuffer(length), 0, length);
+  buffer.setInt8(position, value);
   return buffer;
 }
