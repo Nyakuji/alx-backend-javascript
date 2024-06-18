0ce1d481077c77d250cf8c31d3369dc3ea9412e7
philip Opiyo <opiyophilipo@gmail.com>
2024-06-18 14:22:46 +0300

    refactor: Update stdin.js to use readline module for user input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,11 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Create readline interface for capturing user input
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-
-// Display welcome message
-console.log('Welcome to Holberton School, what is your name?\n');
-
-// Capture user input
-rl.question('', (name) => {
-  // Display user's name
-  console.log(`Your name is: ${name}`);
-
-  // Close the readline interface
-  rl.close();
-});
-
-// Handle program exit
-rl.on('close', () => {
-  console.log('This important software is now closing\n');
-  process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
